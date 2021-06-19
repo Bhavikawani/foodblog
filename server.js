214cd09cf7c77cb323c241d45e6a1cecd63fb05b
Bhavika <wanibhavika007@gmail.com>
2021-06-19 21:13:59 +0530

    Initial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require('express')
 const app = express()
 const path = require("path")
-// const logger = require("morgan")
+
 const mongoose = require("mongoose")
 const session = require("express-session")
 
@@ -12,7 +12,7 @@ const { Console } = require("console")
 
 // app.listen(5000)
 app.use(express.static(path.join(__dirname, "public")))
-// app.use(logger("dev"))
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
